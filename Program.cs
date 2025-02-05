a395ea798536b64e206e1e61654b9769a685da9a
Alexander Vesely <alex.vesely07@gmail.com>
2025-02-05 07:37:09 +0200

    Initial Create

---
+++ Program.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
+namespace SmartLunch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddCookie()
+            .AddOpenIdConnect(options =>
+            {
+                options.Authority = "https://accounts.google.com";
+                options.ClientId = "1019391910323-5s9csp88j1lruboso8kd8ultsvn6vldv.apps.googleusercontent.com";
+                options.ClientSecret = "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";
+                options.CallbackPath = "/signin-oidc";
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+                options.ResponseType = "code";
+                options.SkipUnrecognizedRequests = true;
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
