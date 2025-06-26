b397834a173891a882f526046fc25e1c86cec846
Ghostdog02 <alex.vesely07@gmail.com>
2025-06-26 14:26:17 +0300

    .gitignore is now working

--- UserManagementApi/Program.cs
+++ UserManagementApi/Program.cs
@@ -1,11 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using SmartLunch.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<SmartLunchDbContext>(options =>
+    options.UseSqlServer(builder.Configuration
+                        .GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
